38df4b45d4b769fab867a115ff0ee5919a612403
Pratheush <pratheush@outlook.com>
2022-12-04 11:00:23 +0530

    distributed tracing : custom-span created order-service

--- order-service/src/main/java/com/mylearning/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/mylearning/orderservice/service/OrderService.java
@@ -9,6 +9,8 @@ import com.mylearning.orderservice.repository.OrderRepository;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import brave.Span;
+import brave.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -28,6 +30,17 @@ public class OrderService {
     //private String inventoryUrl="http:'//localhost:8088/api/inventory";
 
     private String inventoryUrl="http://INVENTORY-SERVICE/api/inventory";
+
+    // when we are making a call in a single thread without any creating additional threads we can trace
+    // the request from start to end but if we are trying to create an additional thread on top of the additional thread
+    // then this is not really useful we need to create our own spans.... just like here in this example api-gateway calling order-service
+    // and order-service calling inventory-service this is all in one thread but  when inventory-service is down then inside order-service we used circuit-breaker and for worst case
+    // in order-service retry mechanism and time-out mechanism to call inventory-service but inventory-service has delay due to thread.sleep we implemented
+    // so retry of order-service will retry call to inventory-service and this will be treated as additional thread and so when
+    // order-service is calling inventory-service zipkin is not tracing the call generated from order-service to inventory-service only inventory-service is been traced and registered in zipkin
+    // so we need to create our own span to trace and register it in zipkin
+    // spring-cloud-sleuth provides a mechanism to actually create our own span-ids .. for that we can use Trace class from spring-cloud-sleuth
+    private final Tracer tracer;
 
     //@Autowired
     private final WebClient.Builder webClientBuilder;
@@ -57,22 +70,33 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .collect(Collectors.toList());
 
-        // getting response from Inventory-Service to collect isInStock details for each OrderLineItems of an order
-        InventoryResponse[] responses=webClientBuilder.build().get()
-                        .uri(inventoryUrl,
-                                uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
-                                .retrieve()
-                                        .bodyToMono(InventoryResponse[].class)
-                                                .block();
+        // just before making a call to inventory-service creating Custom Span
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
 
-        // when all the products of the order we place isInStock is true in InventoryResponse then only below line of code will return true
-        boolean allProductsInStock= Arrays.stream(responses).allMatch(InventoryResponse::isInStock);
+        // spring cloud sleuth will assign this particular span-id which we have created in the above step to this particular piece of code which is executed inside below block of code
+        try (Tracer.SpanInScope isLookup = tracer.withSpanInScope(inventoryServiceLookup.start())) {
 
-        if(allProductsInStock){
-            orderRepository.save(order);
-            return "Order Placed Successfully";
-        } else {
-            throw new IllegalArgumentException("Product is not in stock, please try again later");
+            inventoryServiceLookup.tag("call", "inventory-service");
+
+            // getting response from Inventory-Service to collect isInStock details for each OrderLineItems of an order
+            InventoryResponse[] responses = webClientBuilder.build().get()
+                    .uri(inventoryUrl,
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+
+            // when all the products of the order we place isInStock is true in InventoryResponse then only below line of code will return true
+            boolean allProductsInStock = Arrays.stream(responses).allMatch(InventoryResponse::isInStock);
+
+            if (allProductsInStock) {
+                orderRepository.save(order);
+                return "Order Placed Successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+            }
+        }finally {
+            inventoryServiceLookup.flush();
         }
     }
 
